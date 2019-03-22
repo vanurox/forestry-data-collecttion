c9cb75fb4a26057c28b884c59606ce7e800c67e2
cto_varun <cto.varun@gmail.com>
2019-03-22 12:36:58 +0530

    last push

--- helpers/BaseUrl.js
+++ helpers/BaseUrl.js
@@ -1,6 +1,6 @@
 
-// const BaseUrl = "http://forestrydatacollection.com/api/index.php";
-const BaseUrl = "http://192.168.1.36/~nugen-macbook/forestry/api/index.php";
+const BaseUrl = "http://forestrydatacollection.com/api/index.php";
+// const BaseUrl = "http://192.168.1.36/~nugen-macbook/forestry/api/index.php";
 
     
 
